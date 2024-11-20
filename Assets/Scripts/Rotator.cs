fa427c8a0abcab71dbb4a82faf294711ddd456ec
Nikita <93082022+NikitaAnosov@users.noreply.github.com>
2024-11-20 09:18:24 +0200

    Update Rotator.cs

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,12 +5,6 @@ public class Rotator : MonoBehaviour
 
     public float speed = 90f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
